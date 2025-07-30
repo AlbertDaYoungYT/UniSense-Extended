132fa34c02bb01ad144de4e2c1763a92f480326d
Albert Knudsen <kcobain465@gmail.com>
2025-07-30 17:03:09 +0200

    Readding parameters

--- UniSense/LowLevel/DualSenseHIDInputReport.cs
+++ UniSense/LowLevel/DualSenseHIDInputReport.cs
@@ -17,12 +17,6 @@ namespace UniSense.LowLevel
         // The actual data mapping for the touch points (x, y, down, id)
         // is still handled by FieldOffset attributes in DualSenseHIDInputReport.
 
-        [InputControl(name = "primaryTouch", layout = "Touch")]
-        public TouchControl primaryTouch { get; protected set; }
-
-        [InputControl(name = "secondaryTouch", layout = "Touch")]
-        public TouchControl secondaryTouch { get; protected set; }
-
         protected override void FinishSetup()
         {
             base.FinishSetup();
@@ -32,7 +26,7 @@ namespace UniSense.LowLevel
 
         public override Vector2 ReadUnprocessedValueFromState(void* statePtr)
         {
-            if (primaryTouch != null)
+            if (GetChildControl<TouchControl>("primaryTouch") is TouchControl primaryTouch)
                 return primaryTouch.position.ReadValueFromState(statePtr);
             return default;
         }
@@ -148,27 +142,29 @@ namespace UniSense.LowLevel
         // --- Touchpad Input Fields ---
         // These fields are placed in the previously unmapped region of the HID report,
         // specifically bytes 28-47, which aligns with the common DualSense HID report structure.
-        [InputControl(name = "touchpad", layout = "DualSenseTouchpadControl")] // Overall touchpad control group
+        [InputControl(name = "touchpad", layout = "DualSenseTouchpadControl")]
+        [InputControl(name = "touchpad/primaryTouch", layout = "Touch")]
+        [InputControl(name = "touchpad/secondaryTouch", layout = "Touch")]
 
-        [InputControl(name = "touchpad/primaryTouch/position/x", format = "UINT")]
+        [InputControl(name = "touchpad/primaryTouch/position/x", format = "UINT", parameters = "normalize,normalizeMin=0,normalizeMax=1919")]
         [FieldOffset(28)] public uint touchPoint1X;
 
-        [InputControl(name = "touchpad/primaryTouch/position/y", format = "UINT")]
+        [InputControl(name = "touchpad/primaryTouch/position/y", format = "UINT", parameters = "normalize,normalizeMin=0,normalizeMax=1079")]
         [FieldOffset(32)] public uint touchPoint1Y;
 
-        [InputControl(name = "touchpad/primaryTouch/press", layout = "Button")]
+        [InputControl(name = "touchpad/primaryTouch/press", layout = "Button", bit = 0)]
         [FieldOffset(36)] public byte touchPoint1Down; // 0 for up, 1 for down
 
         [InputControl(name = "touchpad/primaryTouch/touchId", format = "BYTE")]
         [FieldOffset(37)] public byte touchPoint1Id;
 
-        [InputControl(name = "touchpad/secondaryTouch/position/x", format = "UINT")]
+        [InputControl(name = "touchpad/secondaryTouch/position/x", format = "UINT", parameters = "normalize,normalizeMin=0,normalizeMax=1919")]
         [FieldOffset(38)] public uint touchPoint2X;
 
-        [InputControl(name = "touchpad/secondaryTouch/position/y", format = "UINT")]
+        [InputControl(name = "touchpad/secondaryTouch/position/y", format = "UINT", parameters = "normalize,normalizeMin=0,normalizeMax=1079")]
         [FieldOffset(42)] public uint touchPoint2Y;
 
-        [InputControl(name = "touchpad/secondaryTouch/press", layout = "Button")]
+        [InputControl(name = "touchpad/secondaryTouch/press", layout = "Button", bit = 0)]
         [FieldOffset(46)] public byte touchPoint2Down; // 0 for up, 1 for down
 
         [InputControl(name = "touchpad/secondaryTouch/touchId", format = "BYTE")]
