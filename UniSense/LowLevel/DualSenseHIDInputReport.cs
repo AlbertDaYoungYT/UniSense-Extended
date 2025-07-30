ed8b2498b5f98d3c9c9bfb2e180441fbe64ac7c9
Albert Knudsen <kcobain465@gmail.com>
2025-07-30 19:27:32 +0200

    A rubber room with rats!

--- UniSense/LowLevel/DualSenseHIDInputReport.cs
+++ UniSense/LowLevel/DualSenseHIDInputReport.cs
@@ -119,8 +119,8 @@ namespace UniSense.LowLevel
         [InputControl(name = "touch0/y", format = "BYTE", parameters = "clampMin=0,clampMax=1079")]
         //[InputControl(name = "touch0/press", offset = 8, layout = "TouchPress", bit = 0)]
         //[InputControl(name = "touch0/touchId", offset = 9, layout = "Integer", format = "BYTE")]
-        [FieldOffset(28)] public short touchPoint1X;
-        [FieldOffset(32)] public short touchPoint1Y;
+        [FieldOffset(34)] public short touchPoint1X;
+        [FieldOffset(38)] public short touchPoint1Y;
         //[FieldOffset(36)] public byte touchPoint1Down; // 0 for up, 1 for down
         //[FieldOffset(37)] public byte touchPoint1Id;
 
@@ -129,8 +129,8 @@ namespace UniSense.LowLevel
         [InputControl(name = "touch1/y", format = "BYTE", parameters = "clampMin=0,clampMax=1079")]
         //[InputControl(name = "touch1/press", offset = 8, layout = "TouchPress", bit = 0)]
         //[InputControl(name = "touch1/touchId", offset = 9, layout = "Integer", format = "BYTE")]
-        [FieldOffset(38)] public short touchPoint2X;
-        [FieldOffset(42)] public short touchPoint2Y;
+        [FieldOffset(44)] public short touchPoint2X;
+        [FieldOffset(48)] public short touchPoint2Y;
         //[FieldOffset(46)] public byte touchPoint2Down; // 0 for up, 1 for down
         //[FieldOffset(47)] public byte touchPoint2Id;
         // --- End Touchpad Input Fields ---
