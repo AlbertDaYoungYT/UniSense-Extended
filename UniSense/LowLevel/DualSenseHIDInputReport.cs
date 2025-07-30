67bf71a4c84cabdec50d25e60e64982af2cfc095
Albert Knudsen <kcobain465@gmail.com>
2025-07-30 18:57:10 +0200

    Added bit size

--- UniSense/LowLevel/DualSenseHIDInputReport.cs
+++ UniSense/LowLevel/DualSenseHIDInputReport.cs
@@ -114,7 +114,7 @@ namespace UniSense.LowLevel
         // These fields are placed in the previously unmapped region of the HID report,
         // specifically bytes 28-47, which aligns with the common DualSense HID report structure.
         //[InputControl(name = "touchpad", layout = "Touchscreen")]
-        [InputControl(name = "touch0", format = "VC2B", layout = "Vector2", displayName = "Touch 0")]
+        [InputControl(name = "touch0", format = "VC2B", layout = "Vector2", sizeInBits = 8, displayName = "Touch 0")]
         [InputControl(name = "touch0/x", offset = 0, layout = "Axis", format = "INT", sizeInBits = 4, parameters = "normalize,normalizeMin=0,normalizeMax=1919")]
         [InputControl(name = "touch0/y", offset = 4, layout = "Axis", format = "INT", sizeInBits = 4, parameters = "normalize,normalizeMin=0,normalizeMax=1079")]
         //[InputControl(name = "touch0/press", offset = 8, layout = "TouchPress", bit = 0)]
@@ -124,7 +124,7 @@ namespace UniSense.LowLevel
         //[FieldOffset(36)] public byte touchPoint1Down; // 0 for up, 1 for down
         //[FieldOffset(37)] public byte touchPoint1Id;
 
-        [InputControl(name = "touch1", format = "VC2B", layout = "Vector2", displayName = "Touch 1")]
+        [InputControl(name = "touch1", format = "VC2B", layout = "Vector2", sizeInBits = 8, displayName = "Touch 1")]
         [InputControl(name = "touch1/x", offset = 0, layout = "Axis", format = "INT", sizeInBits = 4, parameters = "normalize,normalizeMin=0,normalizeMax=1919")]
         [InputControl(name = "touch1/y", offset = 4, layout = "Axis", format = "INT", sizeInBits = 4, parameters = "normalize,normalizeMin=0,normalizeMax=1079")]
         //[InputControl(name = "touch1/press", offset = 8, layout = "TouchPress", bit = 0)]
