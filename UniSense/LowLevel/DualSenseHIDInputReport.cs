134386fa9bea6ecfb35864f93342b337b8064352
Albert Knudsen <kcobain465@gmail.com>
2025-08-05 19:28:33 +0200

    Can i do this???

--- UniSense/LowLevel/DualSenseHIDInputReport.cs
+++ UniSense/LowLevel/DualSenseHIDInputReport.cs
@@ -147,11 +147,11 @@ namespace UniSense.LowLevel
         // These fields are placed in the previously unmapped region of the HID report,
         // specifically bytes 28-47, which aligns with the common DualSense HID report structure.
 
-        [InputControl(name = "touch0", layout = "Touch", layoutType = typeof(DualSenseTouchPoint))]
+        [InputControl(name = "touch0", layout = "Touch", layout = typeof(DualSenseTouchPoint))]
         [FieldOffset(36)]
         public DualSenseTouchPoint touch0;
 
-        [InputControl(name = "touch1", layout = "Touch", layoutType = typeof(DualSenseTouchPoint))]
+        [InputControl(name = "touch1", layout = "Touch", layout = typeof(DualSenseTouchPoint))]
         [FieldOffset(42)]
         public DualSenseTouchPoint touch1;
 
