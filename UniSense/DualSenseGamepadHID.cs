41b131feb8ca51c7b2e6d2664e234f29694dd046
Albert Knudsen <kcobain465@gmail.com>
2025-08-05 21:18:04 +0200

    heheh... forgot a ;

--- UniSense/DualSenseGamepadHID.cs
+++ UniSense/DualSenseGamepadHID.cs
@@ -62,7 +62,7 @@ namespace UniSense
         [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
         static void Initialize()
         {
-            InputSystem.RegisterLayout<DualSenseTouchPoint>()
+            InputSystem.RegisterLayout<DualSenseTouchPoint>();
             InputSystem.RegisterLayout<DualSenseGamepadHID>(
                 matches: new InputDeviceMatcher()
                     .WithInterface("HID")
