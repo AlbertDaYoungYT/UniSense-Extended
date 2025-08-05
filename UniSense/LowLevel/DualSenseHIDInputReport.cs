e286b43958a72f103f5255c15cbcd057810b8076
Albert Knudsen <kcobain465@gmail.com>
2025-08-05 18:07:02 +0200

    test

--- UniSense/LowLevel/DualSenseHIDInputReport.cs
+++ UniSense/LowLevel/DualSenseHIDInputReport.cs
@@ -115,8 +115,8 @@ namespace UniSense.LowLevel
         // specifically bytes 28-47, which aligns with the common DualSense HID report structure.
         //[InputControl(name = "touchpad", layout = "Touchscreen")]
         [InputControl(name = "touch0", layout = "Vector2", format = "VC2S", displayName = "Touch 0")]
-        [InputControl(name = "touch0/x", sizeInBits = 32, parameters = "clampMin=0,clampMax=1919")]
-        [InputControl(name = "touch0/y", sizeInBits = 32, parameters = "clampMin=0,clampMax=1079")]
+        [InputControl(name = "touch0/x", format = "UINT", sizeInBits = 32)]//, parameters = "clampMin=0,clampMax=1919")]
+        [InputControl(name = "touch0/y", format = "UINT", sizeInBits = 32)]//, parameters = "clampMin=0,clampMax=1079")]
         //[InputControl(name = "touch0/press", offset = 8, layout = "TouchPress", bit = 0)]
         //[InputControl(name = "touch0/touchId", offset = 9, layout = "Integer", format = "BYTE")]
         [FieldOffset(32)] public uint touchPoint1X;
@@ -125,8 +125,8 @@ namespace UniSense.LowLevel
         //[FieldOffset(37)] public byte touchPoint1Id;
 
         [InputControl(name = "touch1", layout = "Vector2", format = "VC2S", displayName = "Touch 1")]
-        [InputControl(name = "touch1/x", sizeInBits = 32, parameters = "clampMin=0,clampMax=1919")]
-        [InputControl(name = "touch1/y", sizeInBits = 32, parameters = "clampMin=0,clampMax=1079")]
+        [InputControl(name = "touch1/x", format = "UINT", sizeInBits = 32)]//, parameters = "clampMin=0,clampMax=1919")]
+        [InputControl(name = "touch1/y", format = "UINT", sizeInBits = 32)]//, parameters = "clampMin=0,clampMax=1079")]
         //[InputControl(name = "touch1/press", offset = 8, layout = "TouchPress", bit = 0)]
         //[InputControl(name = "touch1/touchId", offset = 9, layout = "Integer", format = "BYTE")]
         [FieldOffset(42)] public uint touchPoint2X;
